ef441040281053e3c5b8b1521b311fe60e4717c8
Alexander <opera9876@rambler.ru>
2024-03-26 18:20:51 +0300

    feat: input cleaner

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module courses
 
 go 1.22.0
+
+require github.com/bbalet/stopwords v1.0.0
+
+require golang.org/x/text v0.14.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"github.com/bbalet/stopwords"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	useString := flag.Bool("s", false, "get input string")
+	flag.Parse()
+
+	if !(*useString) {
+		fmt.Println("expected for -s flag")
+		return
+	}
+
+	inputString := flag.Args()
+	if len(inputString) == 0 {
+		fmt.Println("expected for non-empty string")
+		return
+	}
+
+	var clearedStrings = clearInput(inputString)
+	fmt.Println(clearedStrings, len(clearedStrings))
+}
+
+func clearInput(inputString []string) []string {
+	var clearedStrings []string
+	for _, str := range inputString {
+		newStr := stopwords.CleanString(str, "en", false)
+		if newStr != " " {
+			clearedStrings = append(clearedStrings, newStr)
+		}
+	}
+	return clearedStrings
 }
