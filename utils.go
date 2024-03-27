dc6936d4105b53fcead49b19573ec2f28ff0184e
Alexander <opera9876@rambler.ru>
2024-03-27 13:10:16 +0300

    add docs for utils

--- utils.go
+++ utils.go
@@ -20,10 +20,18 @@ func getStringFromArguments() ([]string, error) {
 		return []string{}, errors.New("expected for non-empty string")
 	}
 
-	// TODO: может ли быть больше 1 аргумента ? проверить случай с \n
+	// avoid case with other flags and strings
+	if len(inputString) > 1 {
+		return []string{}, errors.New("expected format is \"./myapp -s string_to_stem\"")
+	}
+
 	return strings.Split(inputString[0], " "), nil
 }
 
+// clearInputFromStopWords based on list of words from
+// https://github.com/toadharvard/stopwords-iso/
+//
+// List of words probably will be changed
 func clearInputFromStopWords(inputString []string) []string {
 	var clearedStrings []string
 	stopwordsMapping, _ := sw.NewStopwordsMapping()
