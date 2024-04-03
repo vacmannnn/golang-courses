b82edf592d35256938e4b136187ce111ef23a384
Alexander <opera9876@rambler.ru>
2024-04-03 09:30:50 +0300

    feat: delete comment lines in words.go

--- pkg/words/words.go
+++ pkg/words/words.go
@@ -1,19 +1,16 @@
 package words
 
 import (
-	"fmt"
 	"github.com/kljensen/snowball"
 	"strings"
 )
 
-// StemStringWithClearing
-// TODO: comments on public func
+// TODO: comment on public func
 func StemStringWithClearing(input string) []string {
 
 	splittedString := strings.Split(input, " ")
 
 	var cleanInput = clearInputFromStopWords(splittedString)
-	fmt.Printf("cleared input -- %s\n", cleanInput)
 
 	snowballStemmer := func(input string, language string) (string, error) {
 		return snowball.Stem(input, language, false)
@@ -21,7 +18,6 @@ func StemStringWithClearing(input string) []string {
 
 	var setOfLanguages = []string{"english", "russian"}
 	result := stemInMultipleLanguages(snowballStemmer, cleanInput, setOfLanguages)
-	fmt.Printf("stemmed and cleared input -- %s\n", result)
 
 	return result
 }
