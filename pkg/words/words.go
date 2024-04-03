2e819f340f0e36d7704ebb81dac461319d7b8655
Alexander <opera9876@rambler.ru>
2024-04-03 15:11:52 +0300

    docs: comments, reorder code, delete unused

--- pkg/words/words.go
+++ pkg/words/words.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-// TODO: comment on public func
+// StemStringWithClearing gets string, clears from most popular words and returns slice of keys
 func StemStringWithClearing(input string) []string {
 
 	splittedString := strings.Split(input, " ")
