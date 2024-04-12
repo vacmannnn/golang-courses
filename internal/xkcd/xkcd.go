5a61431a331e4e591c3af17de79bb7a01e458404
Alexander <opera9876@rambler.ru>
2024-04-12 21:46:13 +0300

    feat: delete unused method GetNComics
    made xkcd package more simple, main logic will be moved to main

--- internal/xkcd/xkcd.go
+++ internal/xkcd/xkcd.go
@@ -8,7 +8,6 @@ import (
 	"log"
 	"net/http"
 	"strings"
-	"sync"
 )
 
 type comicsInfo struct {
@@ -26,66 +25,12 @@ type comicsInfo struct {
 }
 
 type ComicsDownloader struct {
-	comicsURL        string
-	comics           map[int]core.ComicsDescript
-	lastDownloadedID int
+	comicsURL string
 }
 
 // NewComicsDownloader sets link to source site with comics
-func NewComicsDownloader(comicsURL string, comics map[int]core.ComicsDescript) ComicsDownloader {
-	return ComicsDownloader{comicsURL: comicsURL, comics: comics}
-}
-
-// ChangeStartIDOfDownloading sets new ID from which newest comics will be downloaded, default value will
-// grow after each download by numOfComics
-func (c *ComicsDownloader) ChangeStartIDOfDownloading(startPos int) {
-	c.lastDownloadedID = startPos
-}
-
-// GetNComicsFromSite gets num of comics that will be downloaded from site. Start id of downloading comics depends
-// on previous downloads. Returns map with needed comics and number of successful downloads.
-func (c *ComicsDownloader) GetNComicsFromSite(numOfComics int) (map[int]core.ComicsDescript, int, error) {
-
-	var resMap = make(map[int]core.ComicsDescript, numOfComics)
-	var (
-		wg                sync.WaitGroup
-		mt                sync.Mutex
-		successDownloaded int
-	)
-
-	for i := c.lastDownloadedID + 1; i <= c.lastDownloadedID+numOfComics; i++ {
-		wg.Add(1)
-		go func(comicsID int) {
-			defer wg.Done()
-			if c.comics[comicsID].Keywords != nil {
-				mt.Lock()
-				defer mt.Unlock()
-				resMap[comicsID] = c.comics[comicsID]
-				successDownloaded++
-				return
-			}
-
-			myComics, _, err := c.GetComicsFromID(comicsID)
-			if err != nil && comicsID != 404 {
-				log.Printf("%s, comicsID is - %d", err, comicsID)
-				return
-			}
-			if comicsID == 404 {
-				successDownloaded++
-				return
-			}
-
-			mt.Lock()
-			defer mt.Unlock()
-			c.comics[comicsID] = myComics
-			resMap[comicsID] = myComics
-			successDownloaded++
-		}(i)
-
-	}
-	wg.Wait()
-	c.lastDownloadedID = c.lastDownloadedID + numOfComics
-	return resMap, successDownloaded, nil
+func NewComicsDownloader(comicsURL string) ComicsDownloader {
+	return ComicsDownloader{comicsURL: comicsURL}
 }
 
 // TODO: descript
