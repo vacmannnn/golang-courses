93ed47a9fc1a080614d1e75fe5a493653eae078e
Alexander <opera9876@rambler.ru>
2024-04-22 14:38:14 +0300

    fix: better cleaner, it removes more 'trash' words
    thanks to @toadharvard for tokenizer

--- pkg/words/words.go
+++ pkg/words/words.go
@@ -7,14 +7,12 @@ import (
 // StemStringWithClearing gets string, clears from most popular words and returns slice of keys
 func StemStringWithClearing(input []string) []string {
 
-	var cleanInput = clearInputFromStopWords(input)
-
+	var cleanInput = clearInput(input)
 	snowballStemmer := func(input string, language string) (string, error) {
 		return snowball.Stem(input, language, false)
 	}
 
 	var setOfLanguages = []string{"english", "russian"}
 	result := stemInMultipleLanguages(snowballStemmer, cleanInput, setOfLanguages)
-
 	return result
 }
