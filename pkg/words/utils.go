1d5af2f3275134c92c082fedfffb6a90b3629e00
Alexander <opera9876@rambler.ru>
2024-04-22 14:38:14 +0300

    fix: better cleaner, it removes more 'trash' words
    thanks to @toadharvard for tokenizer

--- pkg/words/utils.go
+++ pkg/words/utils.go
@@ -2,20 +2,37 @@ package words
 
 import (
 	sw "github.com/toadharvard/stopwords-iso"
+	"regexp"
+	"strings"
 )
 
-// clearInputFromStopWords based on list of words from
-// https://github.com/toadharvard/stopwords-iso/
+// clearInput removes stopwords and marks as "'", "?", "-".
+// stopwords cleaner based on https://github.com/toadharvard/stopwords-iso/
 //
 // List of words probably will be changed
-func clearInputFromStopWords(inputString []string) []string {
-	var clearedStrings []string
+func clearInput(inputString []string) []string {
+	joinedString := strings.Join(inputString, " ")
+	tokenizedString := tokenize(joinedString)
+
 	stopWordsMapping, _ := sw.NewStopwordsMapping()
-	for _, str := range inputString {
+	var clearedString []string
+
+	for _, str := range tokenizedString {
 		newStr := stopWordsMapping.ClearString(str)
-		if newStr != "" {
-			clearedStrings = append(clearedStrings, newStr)
+		if len(newStr) > 2 {
+			clearedString = append(clearedString, newStr)
 		}
 	}
-	return clearedStrings
+	return clearedString
+}
+
+func tokenize(str string) []string {
+	wordSegmenter := regexp.MustCompile(`[\pL\p{Mc}\p{Mn}-_']+`)
+	alphanumericOnly := regexp.MustCompile(`[^\p{L}\p{N} ]+`)
+	words := strings.Fields(str)
+	splitted := strings.Join(words, " ")
+
+	onlyAlphanumeric := alphanumericOnly.ReplaceAllString(splitted, " ")
+	words = wordSegmenter.FindAllString(onlyAlphanumeric, -1)
+	return words
 }
