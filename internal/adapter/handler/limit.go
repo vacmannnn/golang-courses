d0749c6969e16505be7dd592cfb7153ff866ec7d
vacman <opera9876@rambler.ru>
2024-05-20 14:20:05 +0300

    feat: rate limit by user

--- internal/adapter/handler/limit.go
+++ internal/adapter/handler/limit.go
@@ -2,12 +2,39 @@ package handler
 
 import (
 	"golang.org/x/time/rate"
+	"log"
+	"net"
 	"net/http"
+	"sync"
 )
 
-func limit(next http.Handler, bucketSize int) http.Handler {
-	var limiter = rate.NewLimiter(1, bucketSize)
+var visitors = make(map[string]*rate.Limiter)
+var mu sync.Mutex
+
+// maybe inline it ?
+func getVisitor(ip string, bucketLimit int) *rate.Limiter {
+	mu.Lock()
+	defer mu.Unlock()
+
+	limiter, exists := visitors[ip]
+	if !exists {
+		limiter = rate.NewLimiter(1, bucketLimit)
+		visitors[ip] = limiter
+	}
+
+	return limiter
+}
+
+func limit(next http.Handler, bucketLimit int) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			log.Print(err.Error())
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		limiter := getVisitor(ip, bucketLimit)
 		if !limiter.Allow() {
 			http.Error(w, http.StatusText(429), http.StatusTooManyRequests)
 			return
